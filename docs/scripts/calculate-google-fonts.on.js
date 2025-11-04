05e08bfabcc1d549ab4e5fb1e03e1d22043cb1cb
Alan Smith <commit@alanwsmith.com>
2025-11-03 20:00:19 -0500

    getting basic google font pulls

--- docs/scripts/calculate-google-fonts.on.js
+++ docs/scripts/calculate-google-fonts.on.js
@@ -45,15 +45,56 @@ export default class {
       setProp("--adjust-value", this.#adjustment)
       const i = this.#fontIndex;
       const details = fonts[i];
-      console.log(`Checking: ${details[0]}`);
-      const font = new FontFace(details[0], `url("${details[1]}")`);
-       document.fonts.add(font);
-       await font.load();
-       setProp("--test-font", details[0]);
-       await sleep(200);
-       this.#paddedTarget = pad(el.getBoundingClientRect().height);
-       this.api.forward(null, "checkSize");
+      console.log(`Checking: ${details[0]} - ${details[1]}`);
+      if (!this.#data[details[0]]) {
+        this.#data[details[0]] = {};
+      }
+      if (!this.#data[details[0]][details[1]]) {
+        const font = new FontFace(details[0], `url("${details[1]}")`);
+        document.fonts.add(font);
+        await font.load();
+        setProp("--test-font", details[0]);
+        await sleep(200);
+        this.#paddedTarget = pad(el.getBoundingClientRect().height);
+        this.api.forward(null, "checkSize");
+       } else {
+         this.api.forward(null, "rawFont");
+       }
     }
+  }
+
+  async checkSize(_, el) {
+    const currentPadded = pad(el.getBoundingClientRect().height);
+    //console.log(this.#increment);
+    if (currentPadded < this.#paddedTarget) {
+      if (this.#direction === "down") {
+        this.#direction = "up";
+        this.#increment =  this.#increment / 10;
+      }
+      this.#adjustment += this.#increment;
+      //console.log(`Update: ${this.#adjustment}`);
+      setProp("--adjust-value", this.#adjustment);
+      window.requestAnimationFrame((t) => { this.api.forward(null, "checkSize"); });
+    } else if (currentPadded > this.#paddedTarget) {
+      if (this.#direction === "up") {
+        this.#direction = "down";
+        this.#increment =  this.#increment / 10;
+      }
+      this.#adjustment -= this.#increment;
+      setProp("--adjust-value", this.#adjustment);
+      //console.log(`Update: ${this.#adjustment}`);
+      window.requestAnimationFrame((t) => { this.api.forward(null, "checkSize"); });
+    } else {
+      const i = this.#fontIndex;
+      const details = fonts[i];
+      this.#data[details[0]][details[1]] = trimNum(this.#adjustment);
+      this.api.forward(null, "display");
+      this.api.forward(null, "rawFont");
+    }
+  }
+
+  display(_, el) {
+    el.innerHTML = JSON.stringify(this.#data, null, 2);
   }
 
   resetVars() {
