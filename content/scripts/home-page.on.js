6c90c5ed8edb7e7819e03385d267f657b560d6c1
Alan Smith <commit@alanwsmith.com>
2025-11-06 15:07:56 -0500

    working on ui

--- content/scripts/home-page.on.js
+++ content/scripts/home-page.on.js
@@ -1,11 +1,16 @@
 import {matchSorter} from '/scripts/match-sorter.js'
 
-const adjustmentData = [@ json.data["font-size-adjustments"] @].fonts
+const fonts = [@ json.data["font-size-adjustments"] @].fonts
 
 const t = {
-  font: `<div>
-NAME
-</div>`
+  font: `<div>NAME (CATEGORY)</div>`,
+  noMatches: `<div>No Matches</div>`,
+  googleFont: `@font-face { 
+  font-family: "NAME-STYLE";
+  src: url("URL");
+  size-adjust: ADJUST%;
+}
+`,
 }
 
 
@@ -18,24 +23,68 @@ function setProp(key, value) {
 }
 
 export default class {
+  #currentMatch = "";
 
   bittyInit() {
     setProp("--load-hider", "1");
+    this.dev();
   }
+
+  dev() {
+    this.#currentMatch = "Roboto";
+    this.api.forward(null, "styles");
+  }
+
 
   search(event, el) {
     el.replaceChildren();
     const query = event.target.value;
     if (query !== "") {
-      const matches = matchSorter(adjustmentData, query, {
+      const matches = matchSorter(fonts, query, {
         keys: ["name"],
         threshold: matchSorter.rankings.CONTAINS
       });
-      const results = matches.filter((match, index) => index < 7).forEach((match) => {
-        const subs = [["NAME", match.name]];
-        el.appendChild(this.api.makeElement(t.font, subs));
-      });
+      const results = matches.filter((match, index) => index < 7);
+      if (results.length === 0) {
+        el.appendChild(this.api.makeElement(t.noMatches));
+        this.#currentMatch = "";
+      } else {
+        results.forEach((result, resultIndex) => {
+          if (resultIndex === 0) {
+            this.#currentMatch = result.name;
+          }
+          const subs = [
+            ["NAME", result.name],
+            ["CATEGORY", result.category]
+          ];
+          el.appendChild(this.api.makeElement(t.font, subs));
+        });
+      }
+    } else {
+      this.#currentMatch = "";
+    }
+    this.api.forward(null, "styles");
+  }
+
+  styles(_event, el) {
+    if (this.#currentMatch !== "") {
+      const font = fonts.find((font) => font.name === this.#currentMatch);
+      if (font.category === "Google Fonts") {
+        el.replaceChildren(this.getGoogleFont(font));
+      }
+    } else {
+      el.replaceChildren();
     }
   }
 
+  getGoogleFont(font) {
+    return font.styles.map((style) => {
+      return t.googleFont
+        .replace("NAME", font.name)
+        .replace("ADJUST", style.adjust)
+        .replace("STYLE", style.style)
+        .replace("URL", style.path_string)
+        ;
+    }).join("\n");
+  }
 }
