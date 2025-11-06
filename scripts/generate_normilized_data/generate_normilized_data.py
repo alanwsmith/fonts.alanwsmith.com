5d11c1e411946d4b9e4452dd4019a1a1fb7e8604
Alan Smith <commit@alanwsmith.com>
2025-11-06 13:22:50 -0500

    got font size adjustment data

--- scripts/generate_normilized_data/generate_normilized_data.py
+++ scripts/generate_normilized_data/generate_normilized_data.py
@@ -10,7 +10,7 @@ class Maker:
         initial = 0.5 / input * 100
         return float("{:.4f}".format(initial))
 
-    def google_values(self):
+    def get_google_values(self):
         results = []
         for font_name in self.input["google_fonts"]:
             obj = {
@@ -34,14 +34,32 @@ class Maker:
             results.append(obj)
         return results
 
-    def load_data(self, key, path):
-        with open(path) as _in:
-            self.input[key] = json.load(_in) 
-    
-    def nerd_values(self):
+    def get_macos_15(self):
+        results = []
+        fonts = self.input["macos_15"]
+        for font_name in fonts:
+            obj = {
+                    "category": "macOS Fonts",
+                    "name": font_name,
+                    "key": font_name.replace(" ", "_"),
+                    "styles": [
+                        {
+                            "adjust": self.calc_adjust(
+                                fonts[font_name]["value"]
+                            ),
+                            "aspect": fonts[font_name]["value"],
+                            "style": "default",
+                            "path_type": "local",
+                            "path_string": font_name,
+                            }
+                        ]
+                    }
+            results.append(obj)
+        return results
+
+    def get_nerd_values(self):
         results = []
         for font_name in self.input["nerd_fonts"]:
-            print(font_name)
             obj = {
                     "category": "Nerd Fonts",
                     "name": font_name,
@@ -52,23 +70,48 @@ class Maker:
                                 self.input["nerd_fonts"][font_name]
                             ),
                             "aspect": self.input["nerd_fonts"][font_name],
-                            "style": "regular",
+                            "style": "default",
                             "path_type": "download",
                             "path_string": "https://www.nerdfonts.com/font-downloads",
                             }
                         ]
                     }
             results.append(obj)
-
         return results
 
+    def get_windows_10(self):
+        results = []
+        fonts = self.input["windows_10"]
+        for font_name in fonts:
+            obj = {
+                    "category": "Windows Fonts",
+                    "name": font_name,
+                    "key": font_name.replace(" ", "_"),
+                    "styles": [
+                        {
+                            "adjust": self.calc_adjust(
+                                fonts[font_name]["value"]
+                            ),
+                            "aspect": fonts[font_name]["value"],
+                            "style": "default",
+                            "path_type": "local",
+                            "path_string": font_name,
+                            }
+                        ]
+                    }
+            results.append(obj)
+        return results
+
+    def load_data(self, key, path):
+        with open(path) as _in:
+            self.input[key] = json.load(_in) 
 
     def output(self):
         return {
-            "google": self.google_values(),
-            "nerd": self.nerd_values(),
-            "macos_15": {},
-            "window_10": {},
+            "google": self.get_google_values(),
+            "nerd": self.get_nerd_values(),
+            "macos_15": self.get_macos_15(),
+            "window_10": self.get_windows_10(),
         }
 
 if __name__ == "__main__":
@@ -76,7 +119,7 @@ if __name__ == "__main__":
     m.load_data("google_fonts", "../../content/data/aspect-values/google-fonts.json")
     m.load_data("nerd_fonts", "../../content/data/aspect-values/nerd-fonts.json")
     m.load_data("macos_15", "../../content/data/aspect-values/macos-15.json")
-    m.load_data("window_10", "../../content/data/aspect-values/windows-10.json")
+    m.load_data("windows_10", "../../content/data/aspect-values/windows-10.json")
 
     output_path = "../../content/data/font-size-adjustments.json"
     with open(output_path, "w") as _out:
