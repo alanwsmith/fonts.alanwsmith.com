82f9b68c73d0e3e7486ec7c394e5b3a1a6a0836b
Alan Smith <commit@alanwsmith.com>
2025-11-07 08:55:57 -0500

    getting calculated font size

--- docs/scripts/home-page-calculator.on.js
+++ docs/scripts/home-page-calculator.on.js
@@ -18,12 +18,73 @@ export default class {
   #fontName = "";
   #fontURL = "";
 
+  #adjustment = 0.1;
+  #direction = "up";
+  #increment = 0.01;
+  #paddedTarget = null;
+  #data = {};
+
   bittyInit() {
     setProp("--load-hider", "1");
   }
 
-  calculate(_event, _en) {
-    this.api.forward(null, "newStyle");
+  calculate(_event, _el) {
+    if (this.#fontName !== "" && this.#fontURL !== "") {
+      this.api.forward(null, "rawFont");
+    } else {
+      this.api.forward(null, "newStyle");
+    }
+  }
+
+  async checkSize(_, el) {
+    console.log("here1");
+    const currentPadded = pad(el.getBoundingClientRect().height);
+    console.log(this.#increment);
+    console.log(currentPadded);
+    console.log(this.#paddedTarget);
+    if (currentPadded < this.#paddedTarget) {
+      if (this.#direction === "down") {
+        this.#direction = "up";
+        this.#increment =  this.#increment / 10;
+      }
+      this.#adjustment += this.#increment;
+      console.log(`Update: ${this.#adjustment}`);
+      setProp("--adjust-value", this.#adjustment);
+      window.requestAnimationFrame((t) => { this.api.forward(null, "checkSize"); });
+    } else if (currentPadded > this.#paddedTarget) {
+      if (this.#direction === "up") {
+        this.#direction = "down";
+        this.#increment =  this.#increment / 10;
+      }
+      this.#adjustment -= this.#increment;
+      setProp("--adjust-value", this.#adjustment);
+      console.log(`Update: ${this.#adjustment}`);
+      window.requestAnimationFrame((t) => { this.api.forward(null, "checkSize"); });
+    } else {
+      console.log(`Finalized: ${this.#adjustment}`);
+
+
+      // this.#data[fonts[this.#fontIndex][0]] = trimNum(this.#adjustment);
+      //this.api.forward(null, "display");
+      // this.api.forward(null, "rawFont");
+    }
+  }
+
+  // https://fonts.gstatic.com/s/abeezee/v23/esDT31xSG-6AGleN2tCklZUCGpG-GQ.ttf
+  async rawFont(_event, el) {
+    setProp("--adjust-value", this.#adjustment);
+    try {
+      await sleep(400);
+      const font = new FontFace(`font-to-calculate`, `url("${this.#fontURL}")`);
+      document.fonts.add(font);
+      await font.load();
+      await sleep(400);
+      this.#paddedTarget = pad(el.getBoundingClientRect().height);
+      console.log(this.#paddedTarget);
+      this.api.forward(null, "checkSize");
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   fontName(event, _el) {
@@ -41,7 +102,6 @@ export default class {
       el.innerHTML = "Both Font Name and\nFont File URL\nmust be filled out";
     }
   }
-
 
 
   // async rawFont(_, el) {
